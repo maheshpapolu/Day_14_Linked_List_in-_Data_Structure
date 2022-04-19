8e3558976805467af199cf99b9613a87488c69b0
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-19 16:54:50 +0530

    [Mahesh]Delete_any_Node_in_Linked_List

--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace Linked_List
 {
-
-
     internal class Program
     {
-            static void Main(string[] args)
-            {
-                CustomLinkedList<int> list = new CustomLinkedList<int>(); // creating a object of in built LinkedList generic class           
-                list.Append(56); // Calling method of CustomLinkedList generic class to append 56
-                list.Append(30); // Calling method of CustomLinkedList generic class to append 70
-                list.Append(70); // Calling method of CustomLinkedList generic class to append 30 
-                list.DisplayElement(); // Calling method of CustomLinkedList generic class to display linkedlist element
-                list.Search(30); // Calling method of CustomLinkedList generic class to search element in linkedlist
-                list.InsertElement(30, 40);
-                list.DisplayElement();
-                Console.ReadLine();
-            }
+        static void Main(string[] args)
+        {
+            CustomLinkedList<int> list = new CustomLinkedList<int>(); // creating a object of in built LinkedList generic class           
+            list.Append(56); // Calling method of CustomLinkedList generic class to append 56
+            list.Append(30); // Calling method of CustomLinkedList generic class to append 70
+            list.Append(70); // Calling method of CustomLinkedList generic class to append 30 
+            list.DisplayElement(); // Calling method of CustomLinkedList generic class to display linkedlist element
+            list.Search(30); // Calling method of CustomLinkedList generic class to search element in linkedlist
+            list.InsertElement(30, 40);
+            list.DisplayElement();
+            list.DeleteElement(40);
+            list.DisplayElement();
+            Console.ReadLine();
+        }
     }
 }
