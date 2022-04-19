e4c31f1e8c55f3d80683b9282c583b9d341352b3
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-19 16:34:21 +0530

    [Mahesh]Search_Node

--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -10,16 +10,16 @@ namespace Linked_List
 
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            CustomLinkedList<int> list = new CustomLinkedList<int>(); // creating a object of in built LinkedList generic class           
-            list.Append(56); // Calling method of CustomLinkedList generic class to append 56
-            list.Append(30); // Calling method of CustomLinkedList generic class to append 70
-            list.Append(70); // Calling method of CustomLinkedList generic class to append 30 
-            list.DisplayElement(); // Calling method of CustomLinkedList generic class to display linkedlist element
-            list.DeleteAtLast(); // Calling method of CustomLinkedList generic class to delete first element of linkedlist element
-            list.DisplayElement(); // Calling method of CustomLinkedList generic class to display linkedlist element
-            Console.ReadLine();
-        }
+            static void Main(string[] args)
+            {
+                CustomLinkedList<int> list = new CustomLinkedList<int>(); // creating a object of in built LinkedList generic class           
+                list.Append(56); // Calling method of CustomLinkedList generic class to append 56
+                list.Append(30); // Calling method of CustomLinkedList generic class to append 70
+                list.Append(70); // Calling method of CustomLinkedList generic class to append 30 
+                list.DisplayElement(); // Calling method of CustomLinkedList generic class to display linkedlist element
+                list.Search(30); // Calling method of CustomLinkedList generic class to search element in linkedlist
+                list.Search(40); // Calling method of CustomLinkedList generic class to search element in linkedlist
+                Console.ReadLine();
+            }
     }
 }
